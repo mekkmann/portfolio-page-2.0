3ae65aa37f7e88b8fd93062b10c0e064df05080b
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2026-02-27 14:54:27 +0100

    about me links and icons

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,16 +1,28 @@
+import { Github, Linkedin, Mail } from 'lucide-react';
+import Link from 'next/link';
+
 export default function ContactPage() {
   return (
-    <main className="max-w-3xl mx-auto p-6">
-      <h1 className="text-2xl font-semibold mb-4">Contact</h1>
-      <p className="text-gray-700">
-        Email: alex.p.liljekvist@gmail.com
-      </p>
-      <p className="text-gray-700">
-        GitHub: placeholder
-      </p>
-      <p className="text-gray-700">
-        LinkedIn: placeholder
-      </p>
+    <main className="max-w-3xl mx-auto p-6 mt-20 text-lg">
+      <h1 className="text-3xl font-semibold mb-6">Contact</h1>
+      <div className='flex items-center space-x-4 mb-4'>
+        <Mail/>
+        <Link className="text-gray-700 hover:underline" href="mailto:alex.p.liljekvist@gmail.com">
+          alex.p.liljekvist@gmail.com
+        </Link>
+      </div>
+      <div className='flex items-center space-x-4 mb-4'>
+        <Github/>
+        <Link className="text-gray-700 hover:underline" href="https://github.com/mekkmann" target='_blank'>
+          mekkmann
+        </Link>
+      </div>
+      <div className='flex items-center space-x-4 mb-4'>
+        <Linkedin/>
+        <Link className="text-gray-700 hover:underline" href="https://www.linkedin.com/in/pontus-liljekvist-b7224517a/" target='_blank'>
+          Pontus (Alexander) Liljekvist
+        </Link>
+      </div>
     </main>
   );
 }
