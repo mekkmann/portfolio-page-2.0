74dd18f64e6853350387faeea240157f1375b823
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2026-02-27 14:06:56 +0100

    mb for air

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,7 @@ import { projects } from "@/data/projects";
 
 export default function ProjectsPage() {
     return (
-        <main className="max-w-3xl mx-auto p-6">
+        <main className="max-w-3xl mx-auto p-6 mt-20">
             <h1 className="text-2xl font-semibold mb-4">Projects</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 space-y-2">
                 {projects.map((p) => (
