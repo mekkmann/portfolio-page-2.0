6e76dfec4349138298e8dc5fd91bd6d800fad6fa
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2026-02-27 14:10:53 +0100

    text sizing

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,7 +4,7 @@ import { projects } from "@/data/projects";
 export default function ProjectsPage() {
     return (
         <main className="max-w-3xl mx-auto p-6 mt-20">
-            <h1 className="text-2xl font-semibold mb-4">Projects</h1>
+            <h1 className="text-3xl font-semibold mb-4">Projects</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 space-y-2">
                 {projects.map((p) => (
                     <ProjectCard key={p.title} project={p} />
